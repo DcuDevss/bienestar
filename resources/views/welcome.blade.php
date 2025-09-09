f86647fc6f50f4af9877fd3f6bdd6925f7d425f4
DcuDevss <rodri_p86@hotmail.com>
2025-09-09 14:02:31 -0300

    cambio sobre el login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,26 +70,25 @@
                                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
                             </div>
 
-                            <div class="block mt-4  w-fit">
+{{--                             <div class="block mt-4  w-fit">
                                 <label class="text-white" for="remember_me" class="flex items-center">
                                     <x-checkbox id="remember_me" name="remember" />
                                     <span class="ms-2 text-sm text-white">{{ __('Remember me') }}</span>
                                 </label>
-                            </div>
+                            </div> --}}
 
-                            <div class="flex items-center justify-end mt-4">
-                                @if (Route::has('password.request'))
+                            <div class="block mt-4 justify-center">
+                              {{--   @if (Route::has('password.request'))
                                     <a class="underline text-sm text-white hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                                         {{ __('Forgot your password?') }}
                                     </a>
-                                @endif
+                                @endif --}}
 
-                                <x-button class="ms-4">
+                                <x-button class="ms-4 px-6 py-3 text-lg">
                                     {{ __('Log in') }}
                                 </x-button>
                             </div>
                         </form>
-
                     </div>
 
 

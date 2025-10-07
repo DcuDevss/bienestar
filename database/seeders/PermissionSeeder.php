198506879ac4916cf6f898dc304eda4870751921
Cristian <retacris30@gmail.com>
2025-10-07 14:31:30 -0300

    cambios se agrega a reta-nuevo-andorid14

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -23,15 +23,7 @@ class PermissionSeeder extends Seeder
         Permission::create(['name'=>'roles.update','privilege'=>'role edit']);
         Permission::create(['name'=>'roles.destroy','privilege'=>'role delete']);
         Permission::create(['name'=>'roles.show','privilege'=>'role view']);
-       // Permission::create(['name'=>'roles.index','privilege'=>'role list']);
-       // Permission::create(['name'=>'roles.index','privilege'=>'role list']);
-        //Permission::create(['name'=>'roles.index','privilege'=>'role list']);
-        //Permission::create(['name'=>'roles.index','privilege'=>'role list']);
-
-
-        //permisos de usuarios de user
-
-        //Permission::create(['name'=>'users.index','privilege'=>'users panel']);
+      
         Permission::create(['name'=>'users.index','privilege'=>'users list']);
         Permission::create(['name'=>'users.create','privilege'=>'users create']);
         Permission::create(['name'=>'users.store','privilege'=>'users create']);
@@ -39,12 +31,6 @@ class PermissionSeeder extends Seeder
         Permission::create(['name'=>'users.update','privilege'=>'users edit']);
         Permission::create(['name'=>'users.destroy','privilege'=>'users delete']);
         Permission::create(['name'=>'users.show','privilege'=>'users view']);
-
-
-         //permiso de doctor admin
-         //Permission::create(['name'=>'doctor.index','privilege'=>'doctor panel']);
-
-         //Permisos de Oficina
 
          Permission::create(['name'=>'oficinas.index','privilege'=>'oficinas list']);
          Permission::create(['name'=>'oficinas.create','privilege'=>'oficinas create']);
@@ -54,18 +40,13 @@ class PermissionSeeder extends Seeder
          Permission::create(['name'=>'oficinas.destroy','privilege'=>'oficinas delete']);
          Permission::create(['name'=>'oficinas.show','privilege'=>'oficinaa view']);
 
-
-
         $permissionss=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22];
 
         $superAdmin=Role::findByName('super-admin');
         $admin=Role::findByName('admin');
         $superAdmin->givePermissionTo($permissionss);
         $admin->givePermissionTo($permissionss);
-        //$superAdmin=Role::findByName('administrativo');
-        //$superAdmin=Role::findByName('super-admin');
-        //$superAdmin=Role::findByName('super-admin');
-
+     
         //asignacion de permisos de doctor de
         $permissions = [16,17,18,19,20,21,22];
         $doctor = Role::findByName('doctor');
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -27,54 +27,6 @@ class RoleSeeder extends Seeder
         $role10 = Role::create(['name' => 'user_civil']);
 
 
-
-        /* Permission::create(['name'=> 'oficinas.index','description'=>'solo ve el doctor,super-admin,admin-jefe.'])->syncRoles([$role1,$role2,$role4,$role7]);
-       Permission::create(['name'=> 'diadetrabajos.index','description'=>'solo ve el doctor el super-admin, admin-jefe.'])->syncRoles([$role1,$role2,$role4]);
-       Permission::create(['name'=> 'curriculum.index','description'=>'solo ve el doctor y super-admin, admin-jefe.'])->syncRoles([$role1,$role2,$role4]);
-
-       Permission::create(['name'=> 'interviews.index','lo ven todos los roles'])->syncRoles([$role1,$role2,$role3,$role4,$role5,$role6,$role7,$role8]);
-       Permission::create(['name'=> 'disases.index','description'=>'solo ven super-admin, doctor, enfermero'])->syncRoles([$role1,$role2,$role4,$role7]);
-       Permission::create(['name'=> 'multiform.index','description'=>'solo ven algunos '])->syncRoles([$role1,$role2,$role3,$role4,$role5,$role7,$role8]);
-       Permission::create(['name'=> 'patient-certificados.show','description'=>'solo ven algunos'])->syncRoles([$role1,$role2,$role3,$role4,$role5,$role6,$role7]);
-       Permission::create(['name'=> 'patient-enfermedades.show','description'=>'solo ven algunos'])->syncRoles([$role1,$role2,$role4]);
-       Permission::create(['name'=> 'paciente.ver-historial','description'=>'solo ven algunos'])->syncRoles([$role1,$role2,$role3,$role4,$role5,$role6,$role7]);
-      // Permission::create(['name'=> 'indexComisaria1','description'=>'tabla del inventario/solo ven tecnicos-info'])->syncRoles([$role1,$role2]);
-      //Permission::create(['name'=> 'ver-notificaciones','description'=>'tabla notificaciones de trabajo/solo ven tecnicos y admin'])->syncRoles([$role1,$role2,$role3]);
-      // Permission::create(['name'=> 'crear-notificacion','description'=>'crea notificaciones/solo ven tecnicos y admin'])->syncRoles([$role1,$role2]);
-      // Permission::create(['name'=> 'chatlist','description'=>'muestra lista de chat activos'])->syncRoles([$role1,$role2,$role3,$role4,$role5,$role6,$role7,$role8,$role9]);
-      // Permission::create(['name'=> 'userpolicia','description'=>'muestra los usuarios habilitados para el chat'])->syncRoles([$role1,$role2,$role3,$role4,$role5,$role6,$role7,$role8,$role9]);
-          */
-
-
-
-        /*  Route::resource('/roles',RoleController::class)->names('roles');
-
-Route::resource('/users',UserController::class)->names('users');
-
-
-Route::resource('/especialidades',EspecialidadeController::class)->names('especialidades');
-
-
-Route::get('get', function () {
-    return view('doctor.index');
-})->name('doctor.index');
-
-Route::get('/oficinas', OficinaController::class)->name('oficinas.index');
-Route::get('/diadetrabajos', DiadetrabajoController::class)->name('diadetrabajos.index');
-Route::get('/curriculum', [CurriculumController::class, 'index'])->name('curriculum.index');
-Route::get('/interviews/{paciente}', [InterviewController::class, 'index'])->name('interviews.index');
-//Route::get('/disases', Multiform::class)->name('multiform.index');
-Route::get('/disases', DisaseController::class)->name('disases.index');
-Route::get('/multiform', MultiformController::class)->name('multiform.index');
-Route::get('patient/show/{paciente}', PatientHistorialCertificado::class)->name('patient-certificados.show');
-Route::get('patient/edit/{paciente}', PatientHistorialEnfermedades::class)->name('patient-enfermedades.show');
-Route::get('paciente/show/{paciente}', PacienteHistorialGeneral::class)->name('paciente-general.show');
-Route::get('paciente/edit/{paciente}', PacienteEditCertificado::class)->name('paciente-certificado.edit');
-Route::get('/paciente/ver-historial/{paciente}', VerHistorial::class)->name('paciente.ver-historial');
-Route::get('/interviews/detail/{interview}', [InterviewController::class, 'detail'])->name('interviews.detail');
-*/
-
-
         Permission::create(['name' => 'users.index'])->syncRoles([$role1,$role2]);
         Permission::create(['name' => 'users.edit'])->syncRoles([$role1,$role2]);
         Permission::create(['name'=> 'users.update'])->syncRoles([$role1,$role2]);
@@ -84,7 +36,6 @@ Route::get('/interviews/detail/{interview}', [InterviewController::class, 'detai
         Permission::create(['name' => 'roles.edit'])->syncRoles([$role1,$role2]);
         Permission::create(['name'=> 'roles.create'])->syncRoles([$role1,$role2]);
         Permission::create(['name'=> 'roles.show'])->syncRoles([$role1,$role2]);
-
 
 
         Permission::create(['name' => 'oficinas.index'])->syncRoles([$role1, $role2, $role4, $role7]);
@@ -102,11 +53,6 @@ Route::get('/interviews/detail/{interview}', [InterviewController::class, 'detai
         Permission::create(['name' => 'psicologo.index'])->syncRoles([$role1, $role2, $role5]);
 
         Permission::create(['name' => 'enfermero.enfermero-historial'])->syncRoles([$role1, $role2, $role4,$role7]);
-        // Permission::create(['name'=> 'indexComisaria1','description'=>'tabla del inventario/solo ven tecnicos-info'])->syncRoles([$role1,$role2]);
-        //Permission::create(['name'=> 'ver-notificaciones','description'=>'tabla notificaciones de trabajo/solo ven tecnicos y admin'])->syncRoles([$role1,$role2,$role3]);
-        // Permission::create(['name'=> 'crear-notificacion','description'=>'crea notificaciones/solo ven tecnicos y admin'])->syncRoles([$role1,$role2]);
-        // Permission::create(['name'=> 'chatlist','description'=>'muestra lista de chat activos'])->syncRoles([$role1,$role2,$role3,$role4,$role5,$role6,$role7,$role8,$role9]);
-        // Permission::create(['name'=> 'userpolicia','description'=>'muestra los usuarios habilitados para el chat'])->syncRoles([$role1,$role2,$role3,$role4,$role5,$role6,$role7,$role8,$role9]);
-
+      
     }
 }

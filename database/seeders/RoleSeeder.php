0e5c93f4ab373352b5a4a7fdeea38eb11ea90850
Cristian <retacris30@gmail.com>
2025-10-23 22:44:51 -0300

    ggg

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -30,33 +30,32 @@ class RoleSeeder extends Seeder
             $R[$name] = Role::firstOrCreate(['name' => $name, 'guard_name' => 'web']);
         }
 
-        // Sets de permisos por rol (por NOMBRE)patient-certificado.edit
+        // Sets de permisos por rol (por NOMBRE)
         $adminJefe = [
             'users.index','users.edit','users.update',
             'roles.index','roles.edit','roles.create','roles.show',
             'oficinas.index','diadetrabajos.index','curriculum.index',
-            'interviews.index','disases.index','multiform.index','patient-certificado.edit',
-            'patient-certificados.show','patient-enfermedades.show','paciente-certificado.edit',
+            'interviews.index','disases.index','multiform.index',
+            'patient-certificados.show','patient-enfermedades.show','patient-certificado.edit',
             'paciente.ver-historial','psicologo.index','enfermero.enfermero-historial',
         ];
 
-        //
         $administrativo = [
             'oficinas.index','diadetrabajos.index','curriculum.index',
             'interviews.index','disases.index','multiform.index',
             'patient-certificados.show','patient-enfermedades.show',
-            'paciente.ver-historial','patient-certificado.edit',
+            'paciente.ver-historial',
         ];
 
         $doctor = [
             'interviews.index',
             'patient-certificados.show','patient-enfermedades.show','patient-certificado.edit',
-            'paciente.ver-historial','paciente-certificado.edit'
+            'paciente.ver-historial',
         ];
 
         $psico     = ['psicologo.index','interviews.index','paciente.ver-historial'];
         $nutri     = ['interviews.index','paciente.ver-historial'];
-        $enfermero = ['enfermero.enfermero-historial','paciente.ver-historial,'];
+        $enfermero = ['enfermero.enfermero-historial','paciente.ver-historial'];
         $profeGym  = ['interviews.index','paciente.ver-historial'];
 
         // Asignaciones (colecciones por nombre)

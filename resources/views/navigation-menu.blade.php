73fcf9b2e860bdeb9efdb5c193c9c0a1b53c578b
DcuDevss <rodri_p86@hotmail.com>
2025-11-04 12:00:21 -0300

    correcion en el acceso a la ruta

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -275,12 +275,12 @@
                             @endcan
 
                             <!-- Enlace a Estadísticas -->
-                            @role('admin-jefe')
+                            @can('oficinas.index')
                             <a href="{{ route('stats.licencias-stats') }}"
                                 class="block text-sm text-gray-800 hover:bg-slate-300 px-4 py-2 m-0">
                                 Estadísticas
                             </a>
-                            @endrole
+                            @endcan
                         </div>
                     </div>
                 </div>

acbd9d9e5ad3556be60bc3831848abec7cf3876e
DcuDevss <rodri_p86@hotmail.com>
2025-11-07 15:50:38 -0300

    falto en el editar...corregido

--- app/Livewire/Patient/PatientHistorialCertificado.php
+++ app/Livewire/Patient/PatientHistorialCertificado.php
@@ -52,7 +52,7 @@ class PatientHistorialCertificado extends Component
     protected $rules = [
         'name'                           => 'required',
         'fecha_presentacion_certificado' => 'required',
-        'detalle_certificado'            => 'required',
+        'detalle_certificado'            => 'nullable',
         'fecha_inicio_licencia'          => 'required',
         'fecha_finalizacion_licencia'    => 'required|date|after_or_equal:fecha_inicio_licencia',
         'horas_salud'                    => 'nullable|integer',

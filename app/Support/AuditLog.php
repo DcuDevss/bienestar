88d1686f3503c984d4f875e390ea3c55d8e287a7
DcuDevss <rodri_p86@hotmail.com>
2025-11-11 11:49:48 -0300

    cambios para tratar de capturar el ip de la maquina que haga la accion

--- app/Support/AuditLog.php
+++ app/Support/AuditLog.php
@@ -3,6 +3,7 @@
 
 namespace App\Support;
 use App\Models\Audit;
+use Illuminate\Support\Facades\Log;
 
 class AuditLog
 {
@@ -17,14 +18,22 @@ class AuditLog
     {
         $user = auth()->user();
         $req  = request();
-
+    Log::info('IPs debug', [
+        'X-Forwarded-For' => $req->header('X-Forwarded-For'),
+        'X-Real-IP' => $req->header('X-Real-IP'),
+        'CF-Connecting-IP' => $req->header('CF-Connecting-IP'),
+        'ip_detected' => $req->ip(),
+    ]);
         Audit::create([
             'user_id'        => $user?->id,
             'action'         => $action,
             'auditable_type' => $auditable?->getMorphClass(),
             'auditable_id'   => $auditable?->getKey(),
             'description'    => $description,
-            'ip_address'     => $req?->ip(),
+            'ip_address' => $req->header('CF-Connecting-IP')
+                ?: ($req->header('X-Forwarded-For') ? explode(',', $req->header('X-Forwarded-For'))[0] : null)
+                ?: $req->header('X-Real-IP')
+                ?: $req->ip(),
             'user_agent'     => substr((string) $req?->userAgent(), 0, 255),
         ]);
     }
